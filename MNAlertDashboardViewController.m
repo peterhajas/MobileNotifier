3f10ac198ab3f8b80ef9b30b97d3bc84474fd82b
Tim Novinger <tim.novinger@gmail.com>
2011-04-13 00:25:59 -0400

    Sped up performance of dashboard fading
    This slightly increases the speed of the animations of fadeIn and
    fadeOut, and replaces the dashboardBackground with a rendered UIColor
    instead of an image.

--- MNAlertDashboardViewController.m
+++ MNAlertDashboardViewController.m
@@ -65,9 +65,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         alertListView.backgroundColor = [UIColor whiteColor];
         alertListView.layer.cornerRadius = 10;
 		
-		//Dashboard background image
-		dashboardBackground = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,screenBounds.size.width,screenBounds.size.height)];
-		dashboardBackground.image = [UIImage imageWithContentsOfFile:@"/Library/Application Support/MobileNotifier/dashboardBackground.png"];
+		//Dashboard background
+		dashboardBackground = [[UIView alloc] initWithFrame:CGRectMake(0,0,screenBounds.size.width,screenBounds.size.height)];
+		[dashboardBackground setBackgroundColor:[UIColor blackColor]];
 		[dashboardBackground setAlpha:0.75];
 		
 		//ClearAllButton
@@ -190,7 +190,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	dashboardShowing              = NO;
 
 	[UIView beginAnimations:@"fadeDashboardDown" context:NULL];
-	[UIView setAnimationDuration:0.3];
+	[UIView setAnimationDuration:0.25];
 
     [window setFrame:CGRectMake(0,0,320,480)];
     [window setAlpha:0.0];
@@ -238,7 +238,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   clearAllButton.transform          = CGAffineTransformIdentity;
 
 	[UIView beginAnimations:@"fadeIn" context:NULL];
-	[UIView setAnimationDuration:0.3];
+	[UIView setAnimationDuration:0.25];
 
     [window setFrame:CGRectMake(0,-92,320,480)];
     [window setAlpha:1.0];
