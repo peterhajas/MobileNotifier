6619fb0b4c6b7369972208135feee5336fc9ee83
Peter Hajas <peterhajas@gmail.com>
2011-05-28 22:45:57 -0700

    Fixed an issue where the lockscreen would show regardless of the user setting.

--- MNAlertManager.m
+++ MNAlertManager.m
@@ -136,7 +136,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         if ([_delegate deviceIsLocked])
         {
             [lockscreen expandPendingAlertsList];
-            [lockscreen show];
+			[self showLockscreen];
         }
 
         // Start the timer, if the user prefers it
