80e2bca1e772fc2a592c20efe73228a9f71cbf2d
Peter Hajas <peterhajas@gmail.com>
2011-05-28 22:47:28 -0700

    Fixed spacing in MNAlertTableViewDataSourceEditable.m

--- MNAlertTableViewDataSourceEditable.m
+++ MNAlertTableViewDataSourceEditable.m
@@ -4,40 +4,41 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  MNTableViewCell *cell = (MNTableViewCell *) [tableView dequeueReusableCellWithIdentifier:@"notificationTableCell"];
-
-  if (cell == nil)
-  {
-    cell = [[[MNTableViewCell alloc] init] autorelease];
-  }
-
-  MNAlertData *dataObj;
-
-  if(type == kMNAlertTableViewDataSourceTypePending)
-  {
-    dataObj = [[_delegate getPendingAlerts] objectAtIndex:indexPath.row];
-  }
-
-  else if(type == kMNAlertTableViewDataSourceTypeArchived)
-  {
-    dataObj = [[_delegate getDismissedAlerts] objectAtIndex:indexPath.row];
-  }
-
-  else
-  {
-     dataObj = [[[MNAlertData alloc] initWithHeader:@"Null"
-                                           withText:@"Null"
-	                               andSenderAddress:nil
-                                            andType:kPushAlert
-                                        forBundleID:@"com.apple.calculator"
-                                           ofStatus:kNewAlertForeground]
-                                        autorelease];
-  }
-
-  cell.iconImageView.image = [_delegate iconForBundleID:dataObj.bundleID];
-  cell.headerLabel.text = dataObj.header;
-  cell.alertTextLabel.text = dataObj.text;
-  return cell;
+	MNTableViewCell *cell = (MNTableViewCell *) [tableView dequeueReusableCellWithIdentifier:@"notificationTableCell"];
+	
+	if (cell == nil)
+	{
+		cell = [[[MNTableViewCell alloc] init] autorelease];
+	}
+	
+	MNAlertData *dataObj;
+	
+	if(type == kMNAlertTableViewDataSourceTypePending)
+	{
+		dataObj = [[_delegate getPendingAlerts] objectAtIndex:indexPath.row];
+	}
+	
+	else if(type == kMNAlertTableViewDataSourceTypeArchived)
+	{
+		dataObj = [[_delegate getDismissedAlerts] objectAtIndex:indexPath.row];
+	}
+	
+	else
+	{
+		dataObj = [[[MNAlertData alloc] initWithHeader:@"Null"
+	                                          withText:@"Null"
+	                                  andSenderAddress:nil
+	                                           andType:kPushAlert
+	                                       forBundleID:@"com.apple.calculator"
+	                                          ofStatus:kNewAlertForeground]
+	                                       autorelease];
+	}
+	
+	cell.iconImageView.image = [_delegate iconForBundleID:dataObj.bundleID];
+	cell.headerLabel.text = dataObj.header;
+	cell.alertTextLabel.text = dataObj.text;
+	
+	return cell;
 }
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
