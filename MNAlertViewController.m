78b5fa7f7919b3cb3e21dae5ee645dee9b3f6ef6
Frederik Vanggaard <drudoo@drudoo.com>
2011-05-30 11:44:38 -0700

    Added UIKeyboardAppearanceAlert to quickreply.

--- MNAlertViewController.m
+++ MNAlertViewController.m
@@ -219,6 +219,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 		textBox = [[BCZeroEdgeTextView alloc] initWithFrame:CGRectMake(17.0, 155.0, 285.0, 36.0)];
 		[textBox setDelegate:self];
 		textBox.keyboardType = UIKeyboardTypeDefault;
+                textBox.keyboardAppearance = UIKeyboardAppearanceAlert; 
 		textBox.returnKeyType = UIReturnKeyDefault;
 		textBox.font = [UIFont fontWithName:@"HelveticaNeue" size:12.500];
 		textBox.textColor = [UIColor colorWithRed:0.000 green:0.000 blue:0.000 alpha:0.8];
